28e700894c4b5805755b18a3c4cc81d9efb52510
Dmitry Casian <webcasian@gmail.com>
2022-02-26 20:40:52 +0200

    Add arrays to ScriptableObject to store answers data

--- Assets/Scripts/QuestionScriptableObject.cs
+++ Assets/Scripts/QuestionScriptableObject.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionScriptableObject : ScriptableObject
 {
-    [TextArea(2, 6)] 
-    [SerializeField] string question = "Enter new question text here";
+    [TextArea(2, 6)] [SerializeField] private string question = "Enter new question text here";
+    [SerializeField] private string[] answers = new string[4];
+    [Range(0, 3)] [SerializeField] private int correctAnswerIndex;
 
     public string GetQuestion()
     {
         return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
     }
 }
