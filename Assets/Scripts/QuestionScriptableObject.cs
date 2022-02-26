b510231a1e4bc45a2480eacebacbf73fa96fd475
Dmitry Casian <webcasian@gmail.com>
2022-02-26 20:17:44 +0200

    Add ScriptableObject to store questions data

---
+++ Assets/Scripts/QuestionScriptableObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionScriptableObject : ScriptableObject
+{
+    [TextArea(2, 6)] 
+    [SerializeField] string question = "Enter new question text here";
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+}
